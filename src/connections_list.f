0c8ce1bdfcedf80c7d0ab7f38e4070d9867b08fe
Zora V. Dash <zvd@lanl.gov>
2009-04-01 08:58:34 -0600

    Added routine for computing number of connections for each node after connectivity has been simplified based on porosity.

---
+++ src/connections_list.f
@@ -0,0 +1,42 @@
+      subroutine connections_list
+!***********************************************************************
+! Copyright 2006 Los Alamos National Security, LLC  All rights reserved
+! Unless otherwise indicated,  this information has been authored by an 
+! employee or employees of the Los Alamos National Security, LLC (LANS),
+! operator of the  Los  Alamos National  Laboratory  under Contract  No.
+! DE-AC52-06NA25396  with  the U. S. Department  of  Energy.  The  U. S.
+! Government   has   rights  to  use,  reproduce,  and  distribute  this
+! information.  The  public may copy  and  use this  information without
+! charge, provided that this  Notice and any statement of authorship are
+! reproduced on all copies.  Neither  the  Government nor LANS makes any
+! warranty,   express   or   implied,   or   assumes  any  liability  or
+! responsibility for the use of this information.       
+!***********************************************************************
+!D1
+!D1 PURPOSE
+!D1
+!D1 Print out number of connections to a node after simplification 
+!D1 of connectivity based on porosity and terminate execution after
+!D1 list is output.
+!D1
+!***********************************************************************
+
+      use comai, only : neq
+      use combi, only : nelm
+
+      implicit none
+
+      integer i, i1, i2, num, list_unit, open_file
+
+      list_unit = open_file('connections_list.txt', 'unknown')
+
+      do i = 1, neq
+         i1 = nelm(i) + 1
+         i2 = nelm(i + 1)
+         num = i2 - i1
+         write (list_unit, *) num
+      end do
+
+      stop
+
+      end subroutine connections_list
