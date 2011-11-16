a36bbf9ddf2e96c797191969ff479e427a8fbf84
sharad kelkar <kelkar@lanl.gov>
2011-11-16 14:21:04 -0700

    Allow upto 28 edges (8nodes, 7 connections per node ) per element

--- src/setup_edgePointers_3D.f
+++ src/setup_edgePointers_3D.f
@@ -1,34 +1,80 @@
       subroutine setup_edgePointers_3D()
+!***********************************************************************
+! Copyright 2011 Los Alamos National Security, LLC  All rights reserved
+! Unless otherwise indicated,  this information has been authored by an
+! employee or employees of the Los Alamos National Security, LLC (LANS),
+! operator of the  Los  Alamos National  Laboratory  under Contract  No.
+! DE-AC52-06NA25396  with  the U. S. Department  of  Energy.  The  U. S.
+! Government   has   rights  to  use,  reproduce,  and  distribute  this
+! information.  The  public may copy  and  use this  information without
+! charge, provided that this  Notice and any statement of authorship are
+! reproduced on all copies.  Neither  the  Government nor LANS makes any
+! warranty,   express   or   implied,   or   assumes  any  liability  or
+! responsibility for the use of this information.      
+!***********************************************************************
+!
+! Author : Sai Rapaka 
+!
       
       use comai, only: nei, neq
       use combi, only: nelm
       use comfem
       
       integer                            :: el, j, node_I, node_J
+      integer, parameter                 :: numEdges = 28
       
       if(.not. allocated(edges)) then
-        allocate(edges(12, 2))
+        allocate(edges(numEdges, 2))
       endif
 
-      edges( 1,1) = 1; edges( 1,2) = 2
-      edges( 2,1) = 2; edges( 2,2) = 3
-      edges( 3,1) = 3; edges( 3,2) = 4
-      edges( 4,1) = 4; edges( 4,2) = 1
-      edges( 5,1) = 5; edges( 5,2) = 6
-      edges( 6,1) = 6; edges( 6,2) = 7
-      edges( 7,1) = 7; edges( 7,2) = 8
-      edges( 8,1) = 8; edges( 8,2) = 5
-      edges( 9,1) = 3; edges( 9,2) = 7
-      edges(10,1) = 6; edges(10,2) = 2
-      edges(11,1) = 4; edges(11,2) = 8
-      edges(12,1) = 5; edges(12,2) = 1
+!      edges( 1,1) = 1; edges( 1,2) = 2
+!      edges( 2,1) = 2; edges( 2,2) = 3
+!      edges( 3,1) = 3; edges( 3,2) = 4
+!      edges( 4,1) = 4; edges( 4,2) = 1
+!      edges( 5,1) = 5; edges( 5,2) = 6
+!      edges( 6,1) = 6; edges( 6,2) = 7
+!      edges( 7,1) = 7; edges( 7,2) = 8
+!      edges( 8,1) = 8; edges( 8,2) = 5
+!      edges( 9,1) = 3; edges( 9,2) = 7
+!      edges(10,1) = 6; edges(10,2) = 2
+!      edges(11,1) = 4; edges(11,2) = 8
+!      edges(12,1) = 5; edges(12,2) = 1
       
+      edges( 1,1) = 1; edges( 1,2) = 2
+      edges( 2,1) = 1; edges( 2,2) = 3
+      edges( 3,1) = 1; edges( 3,2) = 4
+      edges( 4,1) = 1; edges( 4,2) = 5
+      edges( 5,1) = 1; edges( 5,2) = 6
+      edges( 6,1) = 1; edges( 6,2) = 7
+      edges( 7,1) = 1; edges( 7,2) = 8
+      edges( 8,1) = 2; edges( 8,2) = 3
+      edges( 9,1) = 2; edges( 9,2) = 4
+      edges(10,1) = 2; edges(10,2) = 5
+      edges(11,1) = 2; edges(11,2) = 6
+      edges(12,1) = 2; edges(12,2) = 7
+      edges(13,1) = 2; edges(13,2) = 8
+      edges(14,1) = 3; edges(14,2) = 4
+      edges(15,1) = 3; edges(15,2) = 5
+      edges(16,1) = 3; edges(16,2) = 6
+      edges(17,1) = 3; edges(17,2) = 7
+      edges(18,1) = 3; edges(18,2) = 8
+      edges(19,1) = 4; edges(19,2) = 5
+      edges(20,1) = 4; edges(20,2) = 6
+      edges(21,1) = 4; edges(21,2) = 7
+      edges(22,1) = 4; edges(22,2) = 8
+      edges(23,1) = 5; edges(23,2) = 6
+      edges(24,1) = 5; edges(24,2) = 7
+      edges(25,1) = 5; edges(25,2) = 8
+      edges(26,1) = 6; edges(26,2) = 7
+      edges(27,1) = 6; edges(27,2) = 8
+      edges(28,1) = 7; edges(28,2) = 8
+
       if(.not. allocated(edgeNum1)) then
-        allocate(edgeNum1(nei, 12))
+        allocate(edgeNum1(nei, numEdges))
       endif
       
       if(.not. allocated(edgeNum2)) then
-        allocate(edgeNum2(nei, 12))
+        allocate(edgeNum2(nei, numEdges))
       endif
       
       if(.not. allocated(numElems)) then
@@ -36,15 +82,12 @@
       endif
 
       if(.not. allocated(permFactor)) then
-        allocate(permFactor(nelm(neq+1)))
-      endif
-
-      if(.not. allocated(permFactor_nodal)) then
-        allocate(permFactor_nodal(neq))
+        allocate(permFactor(nelm(neq+1), 3))
+        allocate(permtmp(nelm(neq+1), 3))
       endif
 
       do el=1,nei        ! Loop over elements
-        do j=1,12        ! Loop over edges
+        do j=1,numEdges  ! Loop over edges
           node_I = elnode(el, edges(j,1))
           node_J = elnode(el, edges(j,2))
       
@@ -69,7 +112,7 @@
       numElems = 0
 
       do el = 1, nei
-        do n=1, 12
+        do n=1, numEdges
           n1 = edgeNum1(el, n)
           n2 = edgeNum2(el, n) 
           numElems(n1) = numElems(n1) + 1
@@ -82,22 +125,6 @@
           numElems(n) = -1
         endif
       enddo
-
-      open(37, file='edgeNumbers.dat', status='unknown')
-      write(37, *) 'Edge Number information :'
-      
-      do el=1, nei
-        write(37, '(13I8)') el, edgeNum1(el,:)
-      enddo
- 
-      write(37,*) 
-      write(37,*) 
-
-      do n=1, nelm(neq+1)
-        write(37, *) n, numElems(n)
-      enddo
-
-      close(37)
 
       end subroutine setup_edgePointers_3D
       
