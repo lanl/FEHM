59757fca7ec476da8d03cdd1c71c1cba4599100e
sharad kelkar <kelkar@lanl.gov>
2011-11-16 14:00:11 -0700

    changed tol to 10-20

--- src/harmonic_mean.f
+++ src/harmonic_mean.f
@@ -20,7 +20,7 @@
       real*8 a,b
       real*8 tol
       real*8 harmonic_mean
-      parameter(tol = 1.d-12)
+      parameter(tol = 1.d-20)
 
       harmonic_mean = 2.0d0*a*b/(a + b + tol)
       end function harmonic_mean
