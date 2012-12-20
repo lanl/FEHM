0a545b6ea000233eed635f201055a41f07e17d33
Shaoping Chu <spchu@lanl.gov>
2012-12-20 15:35:52 -0700

    c. modified permmodel=22 to allow use increamenatl shear d. modified permmodel=22 to allow porosity damage

--- src/stressperm_22.f
+++ src/stressperm_22.f
@@ -19,28 +19,36 @@ c mohr-coulomb failure criteria on the plane of maximum shear
       use comsi
       implicit none
 
-      integer jpt,fail_flag
+      integer jpt,fail_flag,iispmd
       real*8  shear_max,stress_norm
       real*8 eigenvec(3,3),alambda(3),rm(3,3)
       real*8 friction,strength
-      real*8 fac(3,3),fac_E(3)
+      real*8 fac(3,3),fac_E(3), fac_por
+      real*8 porosity_damage_factor
 
       fail_flag = 0
       fac = 1.
       fac_E = 1.
+      fac_por=1.
+      iispmd = ispm(jpt) 
       call stressperm_22_failure(jpt,fail_flag,rm)
       if(fail_flag.eq.1) then
          if(itemp_perm22(jpt).eq.0) then
             write(91,*)jpt
             itemp_perm22(jpt) = 1
          endif
-         call stressperm_22_perm(jpt,rm, fac)
+         call stressperm_22_perm(jpt,rm, fac, fac_por)
          call stressperm_22_emod(jpt,rm,fac_E)
 
          pnx(jpt)=pnx0(jpt)*fac(1,1)
          pny(jpt)=pny0(jpt)*fac(2,2)
          pnz(jpt)=pnz0(jpt)*fac(3,3)
-         
+
+         porosity_damage_factor = 1.
+         if(spm11f(iispmd).gt.0.) 
+     &        porosity_damage_factor=1.0 + (spm11f(iispmd)-1.)*fac_por
+         ps(jpt) = min(1.d0, porosity_damage_factor * psini(jpt))
+
          e1(jpt)=e10(jpt)*fac_E(1)
          e2(jpt)=e20(jpt)*fac_E(1)
          e3(jpt)=e30(jpt)*fac_E(1)
@@ -74,6 +82,7 @@ c  y-prime is along the median principal stress
       real*8  shear_max,stress_norm
       real*8 eigenvec(3,3),alambda(3),rm(3,3)
       real*8 friction,strength, pi, pp_fac, cossh, sinsh
+      real*8 stress_factor_initial, shear_threshold
 
       pi=3.1415926535
 
@@ -81,13 +90,24 @@ c  y-prime is along the median principal stress
       friction = spm1f(iispmd)
       strength = spm2f(iispmd)
       pp_fac = spm3f(iispmd)
+      stress_factor_initial = 0.
+      shear_threshold = 0.
+      if(incremental_shear_permmodel.eq.1) then
+         stress_factor_initial = spm12f(iispmd)
+         shear_threshold = spm13f(iispmd)
+      endif
 
 c find the principal stresses. eigenvec(j=1 to 3,k) contains the x,y,z
 c components of the eigenvector for the k-th eigenvalue.  
-      call principal_stress_3D(jpt,alambda,eigenvec)
+      if(incremental_shear_permmodel.eq.1) then
+         call principal_incremental_stress_3D(jpt,alambda,eigenvec
+     &        ,stress_factor_initial)
+      else
+         call principal_stress_3D(jpt,alambda,eigenvec)
+      endif
 c max shear stress,and normal stress on this plane (45deg to principal)
       call max_excess_shear(jpt,friction,strength,alambda,
-     &     pp_fac,eigenvec)
+     &     pp_fac,stress_factor_initial, eigenvec)
       cossh=dcos(shear_angle(jpt))
       sinsh=dsin(shear_angle(jpt))
 c choose z-prime=normal to the failure plane, and 
@@ -103,7 +123,7 @@ c rm(column3)= direction cos of (z-prime) etc.
       rm(2,1)=rm(1,3)*rm(3,1)-rm(3,3)*rm(1,2)
       rm(3,1)=rm(1,3)*rm(2,2)-rm(2,3)*rm(1,2)
 
-      if(excess_shear(jpt).gt.0.0d0) then
+      if(excess_shear(jpt).gt.shear_threshold) then
          fail_flag = 1
       endif
 
@@ -112,7 +132,7 @@ c rm(column3)= direction cos of (z-prime) etc.
 
 c.....................................................................
 
-      subroutine stressperm_22_perm(jpt,rm, fac)
+      subroutine stressperm_22_perm(jpt,rm, fac,fac_por)
 c rotate back the tensor of multiplication factors from the system 
 c of the fault plane to the original axis, multiply permeabilities
       use comdi
@@ -123,7 +143,7 @@ c of the fault plane to the original axis, multiply permeabilities
       integer i,j,k,l,m,n, iispmd
       real*8  perm1,perm2,perm3
       real*8 rm(3,3), rminv(3,3), sum21, sfac
-      real*8 fac(3,3),fac_p(3,3)
+      real*8 fac(3,3),fac_p(3,3), fac_por
 
 c material failed, modify perm and young's modulus. fac1,fac2,fac3 are
 c user specified multiplers, face1 and fac2 are in the plane of the
@@ -143,6 +163,7 @@ c we are rotating back, using the inverse of rm
       enddo
       sfac=abs(excess_shear(jpt)/spm4f(iispmd))
       sfac=min(1.d0,sfac)
+      fac_por=sfac
       fac_p(1,1)=1.0 + (spm8f(iispmd)-1.)*sfac
       fac_p(2,2)=1.0 + (spm9f(iispmd)-1.)*sfac
       fac_p(3,3)=1.0 + (spm10f(iispmd)-1.)*sfac
@@ -198,16 +219,17 @@ c not taking care of the tensor nature of D
 
 c.....................................................................
       subroutine max_excess_shear(jpt,friction,strength,alambda,
-     &     pp_fac,eigenvec)
+     &     pp_fac,stress_factor_initial, eigenvec)
 
       use comsi
-      use comdi, only : phi
+      use comdi
       implicit none
 
       integer jpt,iispmd
       real*8 shear_max ,stress_norm  
       real*8 eigenvec(3,3),alambda(3)
       real*8 friction,strength, pi, pp_fac
+      real*8 stress_factor_initial
 
       pi=3.1415926535
 
@@ -217,7 +239,8 @@ c      strength = spm2f(iispmd)
 
 c Jaeger & Cook, pp 95-96
       shear_max = 0.5*(alambda(3)-alambda(1))
-      stress_norm = 0.5*(alambda(3)+alambda(1))-pp_fac*phi(jpt)
+      stress_norm = 0.5*(alambda(3)+alambda(1))
+     &     -pp_fac*(phi(jpt)-stress_factor_initial*pho(jpt))
 c minimum value of (shear - friction*normal stress)
       excess_shear(jpt) = shear_max*sqrt(1.0d0+friction*friction)
      &     - friction*stress_norm
@@ -228,3 +251,66 @@ c minimum value of (shear - friction*normal stress)
       end
 
 c.....................................................................
+
+      subroutine principal_incremental_stress_3D(i,alambda,eigenvec
+     &        ,stress_factor_initial)
+      use comai
+      use comsi
+
+      implicit none
+      integer i
+      real*8 AMAT(3,3), eigenvec(3,3),alambda(3)
+      real*8 AI1,AI2,AI3
+      real*8 stress_factor_initial
+      real*8 str_init_temp(3,3)
+
+      AMAT=0.0
+      str_init_temp = 0.0
+
+      if (allocated(str_x0_perm)) then
+        str_init_temp(1,1) = stress_factor_initial*str_x0_perm(i) 
+        str_init_temp(2,2) = stress_factor_initial*str_y0_perm(i) 
+        str_init_temp(3,3) = stress_factor_initial*str_z0_perm(i) 
+        str_init_temp(1,2) = stress_factor_initial*str_xy0_perm(i) 
+        str_init_temp(1,3) = stress_factor_initial*str_xz0_perm(i) 
+        str_init_temp(2,3) = stress_factor_initial*str_yz0_perm(i)
+        str_init_temp(2,1) = str_init_temp(1,2)
+        str_init_temp(3,1) = str_init_temp(1,3)
+        str_init_temp(3,2) = str_init_temp(2,3)
+      endif
+
+      AMAT(1,1) = str_x(i) 
+      AMAT(2,2) = str_y(i) 
+      AMAT(3,3) = str_z(i) 
+      AMAT(1,2) = str_xy(i) 
+      AMAT(1,3) = str_xz(i) 
+      AMAT(2,3) = str_yz(i) 
+      AMAT(2,1) = AMAT(1,2)
+      AMAT(3,2) = AMAT(2,3)
+      AMAT(1,3) = AMAT(3,1)
+
+      AMAT = AMAT - str_init_temp
+      
+! Compute the invariants for the matrix
+      AI1 = AMAT(1,1) + AMAT(2,2) + AMAT(3,3)
+      AI2 = AMAT(1,1)*AMAT(2,2) + AMAT(2,2)*AMAT(3,3) 
+      AI2 = AI2 + AMAT(3,3)*AMAT(1,1) - AMAT(1,2)*AMAT(1,2) 
+     &     - AMAT(2,3)*AMAT(2,3)  - AMAT(3,1)*AMAT(3,1)
+      AI3 = AMAT(1,1)*(AMAT(2,2)*AMAT(3,3) - AMAT(2,3)*AMAT(3,2))
+      AI3 = AI3 + AMAT(1,2)*(AMAT(2,3)*AMAT(3,1) - AMAT(2,1)*AMAT(3,3))
+      AI3 = AI3 + AMAT(1,3)*(AMAT(2,1)*AMAT(3,2) - AMAT(3,1)*AMAT(2,2))
+      
+! Solve the characteristic equation for the eigenvalues
+      call solve_cubic(-AI1, AI2,-AI3,alambda)
+      
+! Make the matrix tridiagonal using Householder transformation
+      call reduce_to_tridiagonal(AMAT)
+      
+! Solve for the eigenvectors
+      call eigenvectors(AMAT, alambda, eigenvec)
+      
+      return
+
+      end
+
+c.......................................................................
