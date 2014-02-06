9ea2ea014eb568cd1a0cc2b05048d2638879c081
Dylan Harp <dharp@lanl.gov>
2014-02-06 15:41:04 -0700

    Changed saltvcon test to use f_diff; added saltvcon.xlsx

--- fehmpytests/fehmpytests.py
+++ fehmpytests/fehmpytests.py
@@ -3,11 +3,11 @@ import os,sys
 import re
 from subprocess import call, PIPE
 try:
-    from fpost import *
+    from fdata import *
 except ImportError:
     try:
         sys.path.insert(0,'../pyfehm')
-        from fpost import *
+        from fdata import *
     except ImportError as err:
         print 'ERROR: Unable to import pyfehm fpost module'
         print err
@@ -25,7 +25,7 @@ class Tests(unittest.TestCase):
         ''' Salt variable conductivity
 
             Modification: new Dec 19 2013 dharp@lanl.gov
-            Modification: 
+            Modification: updated to use f_dif function Feb 6 2014
 
             This function tests the saltvcon macro which calculates thermal
             conductivity of crushed and intact salt.
@@ -44,6 +44,9 @@ class Tests(unittest.TestCase):
                 project), EUR 20621, ISBN 92-894-7767-9
             kx = (k_{t-300}/kx_asse)*(300/T)^1.14
             if kx is less then 1.e-6, set to 1.e-6
+
+            The excel spreadsheet in ./saltvcon/saltvcon.xlsx contains the
+            associated calculations.
         '''
         # Relative error theshold
         maxerr = 1.e-4
@@ -51,71 +54,29 @@ class Tests(unittest.TestCase):
         dir = 'saltvcon'
         # Change directory to test directory
         os.chdir(dir)
-        #############################################################
-        # Calculate thermal conductivities
-        T = [20,40,60,80,100,120] # Temperature [C] at nodes
-        phi = [0.01,0.1,0.3,0.5,0.7,0.9] # Porosity
-        Ai = [26.85,5.4,1.14] # Intact salt coefficients
-        Ac = [26.85,1.08,-270,370,-136,1.5,5,1.14] # Crushed salt coefficients
-        #Calculate intact salt thermal conductivities
-        kxi = []
-        for t in T:
-            kxi.append(Ai[1]*((Ai[0]+273.15)/(t+273.15))**Ai[2])
-        #Calculate crushed salt thermal conductivities
-        kxc = []
-        for t,p in zip(T,phi):
-            kx_asse = Ac[2]*p**4+Ac[3]*p**3+Ac[4]*p**2+Ac[5]*p+Ac[6]
-            kx_temp = Ac[1]*kx_asse*((Ac[0]+273.15)/(t+273.15))**Ac[7]
-            if kx_temp < 1.e-6:
-                kx_temp = 1.e-6
-            kxc.append(kx_temp)
-        #############################################################
+        # Read in comparison output file
+        f_old = foutput('intact-compare.out')
         # Run intact salt model
         self.run_fehm('intact.files')
-        # Open FEHM output file and read
-        f = open('intact.out', 'r')
-        data = f.readlines()
-        f.close()
-        os.remove('intact.out')
-        # Search for thermal conductivity column heading 'Kx'
-        for i in range(len(data)):
-            if re.search('Kx',data[i]): break
-        # Collect thermal conductivities
-        kx_sim = []
-        for j in range(6):
-            i += 1
-            kx_sim.append(float(data[i].split()[3])) # Grab values in 4th column for each node 
-        # Compare true and simulated values
-        nodeno = 1
-        for true,sim in zip(kxi,kx_sim):   
-            relerr = abs(true-sim)/true
-            self.assertTrue(relerr<maxerr, '\nIncorrect intact salt thermal conductivity for node '+str(nodeno)+'\nRelative error: '+str(relerr)+', Threshold: '+str(maxerr)+'\nExpected='+str(true)+' Simulated='+str(sim))
-            nodeno += 1
-        #############################################################
-        # Run crushed salt model
+        # Read in simulation output file
+        f_new = foutput('intact.out')
+        f_dif = fdiff(f_new,f_old,components=['water'],variables=['Kx'],format='relative')
+        for n in f_dif.nodes:
+            self.assertTrue(f_dif.node['water'][n]['Kx'][0]<maxerr, '\nIncorrect intact salt thermal conductivity for node '+str(n)+'\nRelative error: '+str(f_dif.node['water'][n]['Kx'][0])+', Threshold: '+str(maxerr)+'\nExpected='+str(f_old.node['water'][n]['Kx'][0])+' Simulated='+str(f_new.node['water'][n]['Kx'][0]))
+        del f_old, f_new, f_dif
+
+        # Read in comparison output file
+        f_old = foutput('crushed-compare.out')
+        # Run intact salt model
         self.run_fehm('crushed.files')
-        # Open FEHM output file and read
-        f = open('crushed.out', 'r')
-        data = f.readlines()
-        f.close()
-        os.remove('crushed.out')
-        # Search for thermal conductivity column heading 'Kx'
-        for i in range(len(data)):
-            if re.search('Kx',data[i]): break
-        # Collect thermal conductivities
-        kx_sim = []
-        for j in range(6):
-            i += 1
-            kx_sim.append(float(data[i].split()[3])) # Grab values in 4th column for each node 
-        # Compare true and simulated values
-        nodeno = 1
-        for true,sim in zip(kxc,kx_sim):   
-            relerr = abs(true-sim)/true
-            self.assertTrue(relerr<maxerr, '\nIncorrect crushed salt thermal conductivity for node '+str(nodeno)+'\nRelative error: '+str(relerr)+', Threshold: '+str(maxerr)+'\nExpected='+str(true)+' Simulated='+str(sim))
-            nodeno += 1
-        #############################################################
+        # Read in simulation output file
+        f_new = foutput('crushed.out')
+        f_dif = fdiff(f_new,f_old,components=['water'],variables=['Kx'],format='relative')
+        for n in f_dif.nodes:
+            self.assertTrue(f_dif.node['water'][n]['Kx'][0]<maxerr, '\nIncorrect intact salt thermal conductivity for node '+str(n)+'\nRelative error: '+str(f_dif.node['water'][n]['Kx'][0])+', Threshold: '+str(maxerr)+'\nExpected='+str(f_old.node['water'][n]['Kx'][0])+' Simulated='+str(f_new.node['water'][n]['Kx'][0]))
+        # Remove files
+        self.cleanup(['nop.temp','fehmn.err','*.avs*','*_head','intact.out','crushed.out'])
         # Return to main directory
-        self.cleanup(['nop.temp','fehmn.err','*.avs*','*_head'])
         os.chdir(self.maindir)
 
     def dissolution(self):
@@ -147,7 +108,7 @@ class Tests(unittest.TestCase):
         # Read in new output files
         f_new = fcontour('dissolution.*_con_node.csv')
         # Diff new and old files
-        f_dif = fdiff(f_old,f_new,variables=['Np[aq] (Moles/kg H20)'])
+        f_dif = fdiff(f_new,f_old,variables=['Np[aq] (Moles/kg H20)'])
         # Test for correct concentrations
         for t in f_dif.times:
             self.assertTrue(f_dif[t]['Np[aq] (Moles/kg H20)'].all()<maxerr, '\nIncorrect concentration at time '+str(t))
@@ -168,7 +129,6 @@ class Tests(unittest.TestCase):
             Cinar, Y, G Pusch and V Reitenbach (2006) Petrophysical and capillary properties of compacted
                 salt. Transport in Porous Media. 64, p. 199-228, doi: 10.1007/s11242-005-2848-1
         '''
-        #############################################################
         cwd = os.getcwd()
         # Relative error theshold
         maxerr = 1.e-4
@@ -176,7 +136,6 @@ class Tests(unittest.TestCase):
         dir = 'salt_perm_poro'
         # Change directory to test directory
         os.chdir(dir)
-        #############################################################
         # Read in comparison files
         f_old = fcontour('compare.00001_sca_node.csv')
         # Run intact salt model
@@ -184,7 +143,7 @@ class Tests(unittest.TestCase):
         # Read in new output files
         f_new = fcontour('run.00001_sca_node.csv')
         # Diff new and old files
-        f_dif = fdiff(f_old,f_new,variables=['n','perm_x'])
+        f_dif = fdiff(f_new,f_old,variables=['n','perm_x'])
         # Test for correct permeabilities
         for node,dif,k_old,k_new in zip(f_dif[1]['n'],f_dif[1]['perm_x'],f_old[1]['perm_x'],f_new[1]['perm_x']):
             self.assertTrue(dif<maxerr, '\nIncorrect permeability at node '+str(node)+'. Expected '+str(k_old)+', Simulated '+str(k_new)) 
