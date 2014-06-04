c0c36e2f79342eca902708ce773bacd7d22a6097
Mark Lange <mlange@lanl.gov>
2014-06-04 14:32:45 -0600

    Modified old2new.py to copy comparison files over.

--- fehmpytests/old2new.py
+++ fehmpytests/old2new.py
@@ -7,12 +7,12 @@ def convert_test(name, new_path):
     
     Automates part of old to new test-case conversion for a given test-case,
     'name', by creating a folder in the new test location, specified by 
-    'new_path', and copying the file-list, input, and grid files. Code 
-    generation will be added soon.
+    'new_path', and copying the control, input, grid, and comparison
+    files. Code generation will be added soon.
     
     *Test-case must be in a single folder in the old suite.
     
-    Modified by mlange806@gmail.com on June 3, 2014. """
+    Modified by mlange806@gmail.com on June 4, 2014. """
 
     #Change directory to specified test case.
     old_path = '/n/swdev/FEHM_dev/VERIFICATION_linux/VERIFICATION_V3.2linux/'
@@ -38,7 +38,6 @@ def convert_test(name, new_path):
                 value = re.sub('N.', '*.', value)
                 value = re.sub('\n', '', value)
                 
-                #Set the entry.
                 file_list[key] = value
             except:
                 #If the line can not be split into kvpairs, ignore.
@@ -64,9 +63,19 @@ def convert_test(name, new_path):
     grid_file = opened_file.read()
     opened_file.close()
     
-    #Get the compare files.
+    #Get the names of the compare files.
     os.chdir('output')
-    file_names = glob('*.avs')
+    types = ['*.avs', '*.his']
+    file_names = []
+    for t in types:    
+        file_names = file_names+glob(t)
+         
+    #Store the contents of each comparision file as a dictionary.
+    comparison_files = {}
+    for f in file_names:
+        opened_file = open(f)
+        comparison_files[f] = opened_file.read()
+        opened_file.close()
     
     #Navigate to fehmpytest.
     os.chdir(new_path)
@@ -96,6 +105,14 @@ def convert_test(name, new_path):
     opened_file = open(file_list['gridf'], 'w')
     opened_file.write(grid_file)
     opened_file.close()
+    
+    #Write the comparison files from the old test suite.
+    for key in comparison_files:
+        opened_file = open('output/'+key, 'w')
+        opened_file.write(comparison_files[key])
+        opened_file.close()
+        
+    print 'Done.'
     
 if __name__ == '__main__':
     #Check that a directory name was specified.
