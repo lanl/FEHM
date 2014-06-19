82d2207797ec97eedf8f8bc4dcc6388b3e3471b4
Shaoping Chu <spchu@lanl.gov>
2014-06-19 14:26:24 -0600

    updated with copyright statement

--- src/subsidence.f
+++ src/subsidence.f
@@ -1,4 +1,24 @@
       subroutine subsidence(iflg)
+!***********************************************************************
+! Copyright 2011. Los Alamos National Security, LLC.  This material was
+! produced under U.S. Government contract DE-AC52-06NA25396 for Los
+! Alamos National Laboratory (LANL), which is operated by Los Alamos
+! National Security, LLC for the U.S. Department of Energy. The U.S.
+! Government has rights to use, reproduce, and distribute this software.
+! Neither the U.S. Government nor Los Alamos National Security, LLC or
+! persons acting on their behalf, make any warranty, express or implied,
+! or assumes any liability for the accuracy, completeness, or usefulness
+! of the software, any information pertaining to the software, or
+! represents that its use would not infringe privately owned rights.
+!
+! The software being licensed may be Export Controlled.   It may not be
+! distributed or used by individuals or entities prohibited from having
+! access to the software package, pursuant to United States export
+! control laws and regulations.  An export control review and
+! determination must be completed before LANS will provide access to the
+! identified Software.
+!***********************************************************************
+
 c      
 c apply freeze-thaw to porosity change  and gridblock thickness
 c
