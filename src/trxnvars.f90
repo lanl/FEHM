8525a18026a16799fa5596d8e8e667d303d10812
Shaoping Chu <spchu@lanl.gov>
2014-11-05 15:06:31 -0700

    Combined lookupvars.f90 module into trxnvars.f90 module to simplify code and avoid potential contingency issues that may arise.

--- src/trxnvars.f90
+++ src/trxnvars.f90
@@ -1,4 +1,4 @@
-module trxnvars ! Basic variables shared among trxn's subroutines
+module trxnvars ! combine original trxnvars and lookupvars
 !***********************************************************************
 ! Copyright 2012. Los Alamos National Security, LLC.  This material was
 ! produced under U.S. Government contract DE-AC52-06NA25396 for Los
@@ -19,6 +19,7 @@ module trxnvars ! Basic variables shared among trxn's subroutines
 ! identified Software.
 !***********************************************************************
 
+! Basic variables shared among trxn's subroutines
 	integer NAME_MAX, SPEC_MAX, LINE_MAX		! Parameters for the maximum space to allocate for different fields.  Probably should get rid of them
 	character*4 aSPEC_MAX, aNAME_MAX
 	parameter ( NAME_MAX = 40 )
@@ -37,5 +38,30 @@ module trxnvars ! Basic variables shared among trxn's subroutines
 	character(20), allocatable :: zonenames(:)	! Zone names
 	logical molein					! Whether we are using the total mole initial water option
 	logical interactive				! Whether we are reading interactively
+
+! lookupvars:  Variables for database lookup subroutine in trxn
+        logical flookup                                                 ! Whether lookup has been called
+        character(LINE_MAX), dimension(SPEC_MAX) :: options             ! Options
+        character(LINE_MAX) database                                    ! Database file name
+        integer dat                                                     ! Filehandle for database
+        integer nlocomp, nlocompaq                                      ! Number of components lookup needs to add
+        character(NAME_MAX), dimension(SPEC_MAX, 3) :: locomp           ! Components to be added by lookup (1 = state, 2 = component, 3 = master)
+        integer ndatcomp                                                ! Number of complexes in the database
+        character(NAME_MAX), allocatable :: datcomp(:), datmaster(:)    ! Complexes in the database and their master species
+        integer ndatcplx, ndatmin, maxdatcplx, maxdatmin                ! Number of complexes and minerals in the database, and the maximum number of reactants and products for each
+        real*8, allocatable :: datcden(:)                               ! Molecular weights in the database
+        character(NAME_MAX), allocatable :: datcplxmain(:)              ! Complex formulae
+        character(NAME_MAX), allocatable :: datcplx(:, :)               ! Species for complexes in the database.  datcplx(i, j) is reaction i, species j
+        real*8, allocatable :: datcplxstoic(:, :)                       ! Stoichiometry for complexes in the database.  Same indexing as datcplx
+        real*8, allocatable :: datcplxlkeq(:)                           ! log_k for complexes in the database
+        real*8, allocatable :: datcplxheq(:)                            ! delta_h for complexes in the database
+        real*8, allocatable :: datcplxtemp(:, :)                        ! Temperature dependency coefficients for complexes in the database
+        character(NAME_MAX), allocatable :: datminnam(:)                ! Names of minerals in the database
+        character(NAME_MAX), allocatable :: datminmain(:)               ! Mineral formulae
+        character(NAME_MAX), allocatable :: datmin(:, :)                ! Species of minerals in the database.  Same indexing as datcplx
+        real*8, allocatable :: datminstoic(:, :)                        ! Stoichiometry for minerals in the database. Same indexing as datcplx
+        real*8, allocatable :: datminlkeq(:)                            ! log_k for minerals in the database
+        real*8, allocatable :: datminheq(:)                             ! delta_h for minerals in the database
+        real*8, allocatable :: datmintemp(:, :)                         ! Temperature dependency coefficients for minerals in the database
 
 end module trxnvars
