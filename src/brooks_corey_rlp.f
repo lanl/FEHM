a2b895d963e7996b4ba47ed648921ffc7639ff36
Shaoping Chu <spchu@lanl.gov>
2017-08-10 15:51:56 -0600

    equations are now consistent with Miller et al. Adv. Water Resourc. (1998), Table 4

--- src/brooks_corey_rlp.f
+++ src/brooks_corey_rlp.f
@@ -1,5 +1,5 @@
-      subroutine brooks_corey_rlp(s, p1, lambda, prop1, dprop11, 
-     &     dprop12, prop2, dprop21, dprop22)
+      subroutine brooks_corey_rlp(s, p1, p2, lambda, q, prop1, dprop11, 
+     &      prop2, dprop21)
 !***********************************************************************
 ! Copyright 2009 Los Alamos National Security, LLC  All rights reserved
 ! Unless otherwise indicated,  this information has been authored by an 
@@ -20,13 +20,14 @@
 ! 2-phase 
 ! input:  s is wetting phase saturation
 !         p1 is residual wetting phase saturation       
+!         p2 is residual non-wetting phase saturation       
 !         lambda (Li and Horne, 2006) 
 ! output: prop1 is rel perm (wetting phase saturation)
 !	  dprop11 is dr/ds
-!	  dprop12 is dr/ds(gas)
+!	  dprop12 is dr/ds(gas) - always 0 if 2-phase
 !	  prop2 is rel perm (non-wetting phase saturation)
 !	  dprop21 is dr/ds
-!	  dprop22 is dr/ds(gas)
+!	  dprop22 is dr/ds(gas) - always 0 if 2-phase
 !***********************************************************************
 
 c      use comdi
@@ -35,34 +36,37 @@ c      use comdi
       implicit none
       real*8 s, p1, p2, lambda, prop1, dprop11, dprop12
       real*8 rp31, sw_star, ds, prop2, dprop21, dprop22
+      real*8 rp32  
+      real*8 q
+ 
+c The Brooks-Corey rel. perm curves follow Miller et al. Adv. Water 
+c Resourc. (1998), Table 4 
+c q is equivalent to 'l' in Miller et al.  Earlier versions of FEHM hard-wired q==2
 
-      rp31 = (2.d0+3.d0*lambda)/lambda
-      sw_star = (s-p1)/(1.d0-p1)
-      ds = 1.d0/(1.d0-p1)
+      rp31 = 1.d0+2.d0/lambda  !exp. in 2nd parentheses in krn (prop2)
+      sw_star = (s-p1)/(1.d0-p1-p2)
+      ds = 1.d0/(1.d0-p1-p2)
 c     calculate rel perms
       if(sw_star.le.0.d0) then
          prop1 = 0.d0
          dprop11 = 0.d0
-         dprop12 = 0.d0
          prop2 = 1.d0
          dprop21 = 0.d0
-         dprop22 = 0.d0
-      elseif(sw_star.ge.1.d0) then
+       elseif(sw_star.ge.1.d0) then
          prop1 = 1.d0
          dprop11 = 0.d0
-         dprop12 = 0.d0
          prop2 = 0.d0
          dprop21 = 0.d0
-         dprop22 = 0.d0
       else
-         prop1 = sw_star**rp31
-         dprop11=rp31*((s-p1)**(rp31-1))/((1-p1)**rp31)
-c     check this wtih gaz, raj
-         dprop12= -1.* dprop11
-         prop2 = (1.d0-sw_star)**2.*(1.0-sw_star**rp31)
-         dprop21=ds*(2.0*(1.0-sw_star)*(1.0-sw_star**rp31)+
-     $        (1.0-sw_star)**2.0*(-rp31*sw_star**(rp31-1.0)))
+         prop1 = sw_star**(q+rp31)
+         dprop11=ds**(q+rp31)*(q+rp31)*(s-p1)**(q+rp31-1.d0)
+         prop2 = ((1.d0-sw_star)**q)*(1.d0-sw_star**rp31)
+! this is derivative of non-wetting rlp with respect to wetting sat         
+         dprop21=ds*(q*(1.d0-sw_star)**(q-1.d0)*(1.d0-sw_star**rp31)+
+     $        rp31*(1.d0-sw_star)**q*sw_star**(rp31-1.d0))
          
       endif
       
       end subroutine brooks_corey_rlp
+
+
