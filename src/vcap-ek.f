d701f04c5eddc61950a8c844b10d91218c26463a
Shaoping Chu <spchu@lanl.gov>
2017-08-10 15:54:03 -0600

    added comment lines, re-arranged code so that it's more modular.

--- src/vcap-ek.f
+++ src/vcap-ek.f
@@ -17,10 +17,10 @@
 ! this calculates capillary pressure (hp) as a function of liquid saturation (sl)
 ! slr = minimum residual saturation
 ! smr = maximum residual saturation
-! alpha, beta are defined in other vgcap subroutines
+! alpha is 1/m (inverse air entry pressure) = Pa/(density*g)
+! beta is 'm' in VG 1980 paper and 'n' in FEHM MMS description
 ! smcut, sucut are limits of the function -  if sl< smcut or sl > sucut,
 ! linear interpolation is used.  
-! cp(sl=0) = 2 X cp(sl=smcut)
 ! subroutine returns hp (capillary pressure) and dhp (dhp/dsl)
 ! 
       implicit none
@@ -30,68 +30,58 @@
       real*8 smr
       real*8 alpha
       real*8 beta
-      real*8 ac1
-      real*8 ac2
-      real*8 ac3
-      real*8 ac4
-      real*8 bc3
-      real*8 bc4
       real*8 smcut
       real*8 sucut
       real*8 hp
       real*8 dhp,star2,hp_cut
-
-
-      real*8 alamda
+      real*8 lambda
       real*8 ds
       real*8 denom
       real*8 star
-      real*8 alpi
-      real*8 termstar1
-      real*8 termstar2
-      real*8 termb1
-      real*8 termb2
-      alamda = 1.0-1.0/beta
-      alpi = 1.0/alamda
+     
+      lambda = 1.0-1.0/beta
       denom = smr-slr
-      star=(sl-slr)/denom
       ds = 1.0/denom
       if(sl.gt.smcut.and.sl.lt.sucut) then
-         termstar1 = star**alpi
-         termstar2 = star*termstar1
-         termb1 = 1./termstar1 - 1.
-         termb2 = termb1**(-alamda)
-         hp = 1./alpha * termb1*termb2
-         dhp = (1.-alamda)/alpha * termb2 * (-alpi/termstar2) * ds
+      	 star=(sl-slr)/denom
+      	 call cc(star,lambda,alpha,denom,hp,dhp)
       elseif(sl.le.smcut)  then
-         star2=(smcut-slr)/denom
-         termstar1 = star2**alpi
-         termstar2 = star2*termstar1
-         termb1 = 1./termstar1 - 1.
-         termb2 = termb1**(-alamda)
-         hp_cut = 1./alpha * termb1*termb2
+         star=(smcut-slr)/denom
+
+       	 call cc(star,lambda,alpha,denom,hp_cut,dhp)
          dhp=hp_cut/smcut
          hp=hp_cut+(smcut-sl)*dhp
          dhp=-1.*dhp
       else
-         star2=(sucut-slr)/denom
-         termstar1 = star2**alpi
-         termstar2 = star2*termstar1
-         termb1 = 1./termstar1 - 1.
-         termb2 = termb1**(-alamda)
-         hp_cut = 1./alpha * termb1*termb2
+      
+         star =(sucut-slr)/denom
+         call cc(star,lambda,alpha,denom,hp_cut,dhp)
          dhp=hp_cut/(1.d0-sucut)
          hp=hp_cut-(sl-sucut)*dhp
          dhp=-1.*dhp
       endif
-c      if(sl.lt.0.0.or.sl.gt.1.0) then
-      if(sl.lt.1.d-6 .or. sl.gt.1.0) then
-c keating 2012 -  these last few lines were forcing cp=0 at sl=0  - I commented them out
-c 
-c      if( sl.gt.1.0) then
-c     lower residual cutoff
-c         hp = 0.
-c         dhp= 0.0
-      endif
+!      write(*,*) 'line 63 ',sl,slr,smr,alpha,beta,hp
+      
       return
       end
+      subroutine cc(star,lambda,alpha,denom,hp,dhp)
+      implicit none
+      real*8 star,lambda,alpha,denom,hp,chp,termstar1,termstar2,termb1
+      real*8 termb2,dhp
+         termstar1 = star**(1./lambda)
+         termstar2 = star*termstar1
+         termb1 = 1./termstar1 - 1.
+         termb2 = termb1**(-lambda)  
+         hp = 1./alpha * termb1*termb2  
+         dhp = (1.-lambda)/alpha * termb2 / (-1.*termstar2*lambda*denom)
+         return
+         end
+      
+      
+      
+      
+      
+      
+      
+      
+      
